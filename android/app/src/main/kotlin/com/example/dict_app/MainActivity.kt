bec2a711ee289848f600cf7104c742df476c3de4
itsmedona <donashaji254@gmail.com>
2024-11-06 17:00:34 +0530

    project started

---
+++ android/app/src/main/kotlin/com/example/dict_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dict_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'homescreen/homescreen.dart';
+
+void main() {
+  runApp(DictApp());
+}
+
+class DictApp extends StatelessWidget {
+  const DictApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
