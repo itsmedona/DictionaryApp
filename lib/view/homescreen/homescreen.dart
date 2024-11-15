26ee99a1b2dd356b88aaebd674c70b9ab16e0d69
itsmedona <donashaji254@gmail.com>
2024-11-15 17:29:58 +0530

    homescreen updated

--- lib/view/homescreen/homescreen.dart
+++ lib/view/homescreen/homescreen.dart
@@ -89,6 +89,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.green,
           title: Text(
             "Dicto Chat",
             style: GoogleFonts.poppins(
